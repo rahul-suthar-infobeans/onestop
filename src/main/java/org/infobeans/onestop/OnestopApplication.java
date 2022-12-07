5211e532ec24d99806150e1176e6698336f27336
rahul-suthar-infobeans <rahul.suthar.external@germanedge.com>
2022-12-07 13:12:01 +0530

    Project init
    project initialized with basic dependency for the project

---
+++ src/main/java/org/infobeans/onestop/OnestopApplication.java
@@ -0,0 +1,13 @@
+package org.infobeans.onestop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnestopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnestopApplication.class, args);
+	}
+
+}
